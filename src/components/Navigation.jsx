ed1702ba0d2834f4496698f4f066993d08f61238
Marvin Berrio <marvinccp@gmail.com>
2023-01-13 00:25:23 +0100

    create a base project and fetching

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Navigation.css'
+const Navigation = () => {
+  return (
+    <div className='nav-container'>
+        <h2>Mil-recetas</h2>
+        </div>
+  )
+}
+
+export default Navigation
+
