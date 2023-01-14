eed3525ce3247a7e02d3806537a2dbe872d53707
Marvin Berrio <marvinccp@gmail.com>
2023-01-14 23:28:57 +0100

    recipes fetch only letter a

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import '../styles/Navigation.css'
 const Navigation = () => {
   return (
-    <div className='nav-container'>
-        <h2>Mil-recetas</h2>
-        </div>
-  )
+    <div className="nav-container">
+      <h2>Mil-recetas</h2>
+      <ul>
+        <a href="/">
+          <li>Home</li>
+        </a>
+        <a href="/recipes">
+          <li>Recipes</li>
+        </a>
+      </ul>
+    </div>
+  );
 }
 
 export default Navigation
