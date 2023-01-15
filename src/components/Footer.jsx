b9d180b909f15c9af4fe451b29f38dbdd0dda1ef
Marvin Berrio <marvinccp@gmail.com>
2023-01-15 18:48:06 +0100

    styles 70%

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+        <h5>Marvin Berrio @2023 Made with Astro</h5>
+    </div>
+  )
+}
+
+export default Footer
